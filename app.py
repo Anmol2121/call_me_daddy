4759e7257e6c74a9464680ebbbd2161c5d17d72d
Anmol Chaudhary <67506322+Anmol2121@users.noreply.github.com>
2026-04-03 21:25:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ login_manager.login_view = 'login'
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
 app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', 'true').lower() == 'true'
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'supportyourerp@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'rsgfbydmxqefdrze')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'c9748268@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'uzemporsbfeesjpy')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', 'noreply@schoolerp.com')
 
 from email.mime.text import MIMEText
